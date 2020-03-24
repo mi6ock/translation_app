e6b8f1e13114a8a6e98deda1c11f8a76bd5a5309
pandsmed <amemiya.pst@gmail.com>
2020-03-24 16:43:27 +0900

    fix swap logic

--- lib/notifier/language_notifier.dart
+++ lib/notifier/language_notifier.dart
@@ -29,6 +29,8 @@ class LanguageNotifier extends ChangeNotifier {
 
   /// 言語コードの順番を入れ替える
   ///
+  /// 入れ替える必要がなければ、入れ替わらない
+  ///
   /// [languageCode]を[isLeft]がtrueであれば1番目に、
   /// falseであれば2番目に格納する
   ///
@@ -41,6 +43,10 @@ class LanguageNotifier extends ChangeNotifier {
     int indexMyLocale =
         sortedLanguages.indexOf(languageCode);
     if (isLeft) {
+      if (languageCode == sortedLanguages[0]) {
+        return;
+      }
+
       if (languageCode == sortedLanguages[1]) {
         sortedLanguages.insert(
             0, sortedLanguages.removeAt(indexMyLocale));
